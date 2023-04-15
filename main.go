96513fed39f91f9148bb6bb7b963d616c8da3a1c
The-Estonian <Zaar2213@gmail.com>
2023-04-15 19:13:33 +0300

    created main.go file and preliminary starting code

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "github.com/The-Estonian/Calculator_in_Go/basic"
+    "github.com/The-Estonian/Calculator_in_Go/advanced"
+)
+
+func main() {
+    var num1 int = 1
+    var num2 int = 2
+    
+    product := basic.Multiplication(num1, num2)
+    square := advanced.Square(num2)
+
+    fmt.Printf("The product of %d and %d is %d\n", num1, num2, product)
+    fmt.Printf("The square of %d is %d\n", num2, square)
+}
